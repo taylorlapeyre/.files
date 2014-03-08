dd3ac1bd56ac7f0fdfe2d09f0631d5d7c38c8faf
Taylor Lapeyre <taylorlapeyre@gmail.com>
2014-03-08 17:33:46 -0600

    fine tune bootstrap script

--- osx/bootstrap.sh
+++ osx/bootstrap.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 function install_brews() {
-	echo "I'm going to update homebrew's packages. Type 'ok' to continue."
-	read RESPONSE
-	if [[ "$RESPONSE" == "ok" ]]; then
-		brew update
-		brew upgrade
-	else
-		exit
-	fi
+  echo "I'm going to update homebrew's packages. Type 'ok' to continue."
+  read RESPONSE
+  if [[ "$RESPONSE" == "ok" ]]; then
+    brew update
+    brew upgrade
+  else
+    exit
+  fi
 
-  brew_install() {
+  o() {
     echo
     echo "${*:2}"
     echo "- Do you want to install $(tput setaf 2)$1$(tput sgr0)? y/n"
@@ -21,27 +21,27 @@ function install_brews() {
     fi
   }
 
-	brew_install "libtool libxslt libksba openssl libyaml" \
-	"There are some basic C libraries for development that I think you should have."
+  o "libtool libxslt libksba openssl libyaml" \
+    "There are some basic C libraries for development that I think you should have."
 
-	brew_install "git" \
-	"Git is a distributed version control system."
+  o "git" \
+    "Git is a distributed version control system."
 
-	brew_install "vim" \
-	"Vim is the de-facto Unix text editor."
+  o "vim" \
+    "Vim is the de-facto Unix text editor."
 
-	brew_install "node" \
-	"Node is a platform built on Chrome's JavaScript runtime."
+  o "node" \
+    "Node is a platform built on Chrome's JavaScript runtime."
 
-	brew_install "python" \
-	"Python is probably out of date."
+  o "python" \
+    "Python is probably out of date."
 
-	brew_install "chruby ruby-install" \
-	"It's generally a bad idea to use the system ruby. \n" \
-	"chruby is a good way to manage ruby."
+  o "chruby ruby-install" \
+    "It's generally a bad idea to use the system ruby. \n" \
+    "chruby is a good way to manage ruby."
 
-	brew_install "stow" \
-	"GNU Stow is a useful utility for installing dotfiles."
+  o "stow" \
+    "GNU Stow is a useful utility for installing dotfiles."
 
   echo
   echo "This script can install GUI-based user applications, such as Google Chrome."
@@ -51,43 +51,43 @@ function install_brews() {
     brew tap phinze/homebrew-cask 2> /dev/null
     brew install brew-cask 2> /dev/null
 
-		brew_cask_install() {
-			echo
-			echo "${*:2}"
-			echo "- Do you want to install $(tput setaf 2)$1$(tput sgr0)? y/n"
-			read RESPONSE
-			if [[ "$RESPONSE" == "y" ]]; then
-					brew cask install $1 2> /dev/null
-				echo "Installed $1"
-			fi
-		}
+    o() {
+      echo
+      echo "${*:2}"
+      echo "- Do you want to install $(tput setaf 2)$1$(tput sgr0)? y/n"
+      read RESPONSE
+      if [[ "$RESPONSE" == "y" ]]; then
+          brew cask install $1 2> /dev/null
+        echo "Installed $1"
+      fi
+    }
 
-		brew_cask_install "sublime-text" \
-		"Sublime Text is a sophisticated text editor for code, markup and prose."
+    o "sublime-text" \
+      "Sublime Text is a sophisticated text editor for code, markup and prose."
 
-		brew_cask_install "dropbox" \
-		"Dropbox is a cloud-based file hosting solution."
+    o "dropbox" \
+      "Dropbox is a cloud-based file hosting solution."
 
-		brew_cask_install "google-chrome" \
-		"Google Chrome is a popular and modern web browser."
+    o "google-chrome" \
+      "Google Chrome is a popular and modern web browser."
 
-		brew_cask_install "screenhero" \
-		"Screenhero is screen sharing made for remote developers"
+    o "screenhero" \
+      "Screenhero is screen sharing made for remote developers"
 
-		brew_cask_install "virtualbox vagrant" \
-		"Vagrant is a tool for building and distributing working environments."
+    o "virtualbox vagrant" \
+      "Vagrant is a tool for building and distributing working environments."
 
-		brew_cask_install "silverlight" \
-		"Silverlight is an awful browser plug-in required for watching Netflix."
+    o "silverlight" \
+      "Silverlight is an awful browser plug-in required for watching Netflix."
 
-		brew_cask_install "rdio" \
-		"Rdio is an ad-free music subscription service."
+    o "rdio" \
+      "Rdio is an ad-free music subscription service."
 
-		brew_cask_install "alfred" \
-		"Alfred is an award-winning productivity application for Mac OS X."
+    o "alfred" \
+      "Alfred is an award-winning productivity application for Mac OS X."
 
-		brew_cask_install "nvalt" \
-		"Notational Velocity is a fast and lightweight note taking application."
+    o "nvalt" \
+      "Notational Velocity is a fast and lightweight note taking application."
   fi
 }
 
@@ -107,22 +107,21 @@ function configure_settings() {
 }
 
 function install_brew() {
-	echo
-	echo "Looks like you're brand new! Welcome to this bootstrap script."
-	echo "I'm not going to install anything onto your system without your explicit permission."
-	echo "furthermore, the only way I will install things is via homebrew."
-	echo "First things first, I'm going to install $(tput setaf 2)homebrew$(tput sgr0)."
-	echo "Type 'ok' to continue."
-	read RESPONSE
-	if [[ "$RESPONSE" == "y" ]]; then
-		ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-	fi
+  echo
+  echo "Looks like you're brand new! Welcome to this bootstrap script."
+  echo "I'm not going to install anything onto your system without your explicit permission."
+  echo "furthermore, the only way I will install things is via homebrew."
+  echo "First things first, I'm going to install $(tput setaf 2)homebrew$(tput sgr0)."
+  echo "Type 'ok' to continue."
+  read RESPONSE
+  if [[ "$RESPONSE" == "y" ]]; then
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+  fi
   echo
   echo "A$(tput setaf 3)ATTENTION:$(tput sgr0)"
   echo "Since this is the first time homebrew is being installed, I'm going to run brew doctor for you."
-  echo "Run this script again after any issues are resolved."
+  echo "Run this script again after any issues are resolved. Running brew doctor in..."
   echo
-s
   for COUNT in {5..0}; do
     echo -n "$COUNT "
     sleep 1
@@ -130,7 +129,7 @@ s
   brew doctor
   echo
   echo "Message from the script:"
-  echo "You'll probably want to edit $(tput setaf 3)/etc/paths$(tput sgr0) to put $(tput setaf 3)/usr/local/bin$(tput sgr0) first."
+  echo "You'll especially want to edit $(tput setaf 3)/etc/paths$(tput sgr0) to put $(tput setaf 3)/usr/local/bin$(tput sgr0) first."
   echo
   exit
 }
